13ebf228cfda14a3f8714e0eee5565eefadf5285
Ilya Kantor <iliakan@gmail.com>
2020-03-23 11:22:20 +0300

    filter future orders

--- libs/db.js
+++ libs/db.js
@@ -2,10 +2,18 @@ let Db = require('@javascriptru/json-db');
 let config = require('../config');
 let path = require('path');
 
-let db = new Db({
+class BackendDb extends Db {
+  load() {
+    super();
+    this.data.orders = this.data.orders.filter(order => order.createdAt < Date.now());
+  }
+}
+
+let db = new BackendDb({
   dataPath: path.resolve(config.dataRoot, 'db.json'),
   schemasPath: path.resolve(config.dataRoot, 'schemas.js')
 });
+
 
 db.load();
 
