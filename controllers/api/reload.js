feee5ff18e3e8739874b983960490ff57ffd89e3
Ilya Kantor <iliakan@gmail.com>
2020-03-23 09:58:47 +0300

    Revert "WIP"
    This reverts commit 75bef0bc112a4ca9853b7d7820b2845f45a374c0.

--- controllers/api/reload.js
+++ controllers/api/reload.js
@@ -1,12 +1,6 @@
 let db = require('../../libs/db');
-let fs = require('fs');
-let config = require('../../config');
-const { resolve } = require('path');
 
 module.exports = async (ctx) => {
-
-  fs.copyFileSync(resolve(config.dataRoot, 'db.json'), resolve(config.dataRoot, 'db.work.json'));
-
   db.load();
   ctx.body = "Database reloaded.";
 };
--- libs/db.js
+++ libs/db.js
@@ -1,13 +1,10 @@
 let Db = require('@javascriptru/json-db');
 let config = require('../config');
-let { resolve } = require('path');
-let fs = require('fs');
-
-fs.copyFileSync(resolve(config.dataRoot, 'db.json'), resolve(config.dataRoot, 'db.work.json'));
+let path = require('path');
 
 let db = new Db({
-  dataPath: resolve(config.dataRoot, 'db.work.json'),
-  schemasPath: resolve(config.dataRoot, 'schemas.js')
+  dataPath: path.resolve(config.dataRoot, 'db.json'),
+  schemasPath: path.resolve(config.dataRoot, 'schemas.js')
 });
 
 db.load();
