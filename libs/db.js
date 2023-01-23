245ecba71020f627129bfdfc5e80a62d7ec83869
Ilya Kantor <iliakan@gmail.com>
2023-01-23 11:14:18 +0100

    db: order.createdAt from today back

--- libs/db.js
+++ libs/db.js
@@ -5,7 +5,22 @@ let path = require('path');
 class BackendDb extends Db {
   load() {
     super.load();
-    this.data.orders = this.data.orders.filter(order => order.createdAt < Date.now());
+
+    let maxDateOrder = this.data.orders.reduce((prev, current) => (prev.createdAt < current.createdAt ? current : prev), { createdAt: 0 });
+
+    let dateDiff = new Date() - maxDateOrder.createdAt;
+
+    // console.log(dateDiff);
+
+    // console.log(maxDateOrder);
+
+    for(let order of this.data.orders) {
+      order.createdAt = new Date(+order.createdAt + dateDiff);
+    }
+
+    // console.log(this.data.orders.reduce((prev, current) => (prev.createdAt < current.createdAt ? current : prev), { createdAt: 0 }));
+
+    // this.data.orders = this.data.orders.map(order => order.createdAt < Date.now());
   }
 }
 
